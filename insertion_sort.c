b7744db65c5a811fd053cf9793f8154d6bd7cf08
Eren Bülbül <97095537+ebx0@users.noreply.github.com>
2023-09-11 15:57:01 +0300

    Add 'initApartmentStruct' function

--- insertion_sort.c
+++ insertion_sort.c
@@ -6,74 +6,112 @@
 typedef unsigned int dword;
 typedef unsigned char byte;
 
-/** PROJECT VARIABLES **/
-int arr[] = {8, 15, 2, 4, 20, -7, 5, 9, 14, 11};
-size_t arrLength = (sizeof(arr) / sizeof(arr[0]));
-
 /** ERROR HANDLING **/
 enum errorCodes {
-    ERR_EMPTY_ARRAY = 1,
-    ERR_last, // Only for indexing, do not use or delete
+  NO_ERROR = 0,
+  ERR_INVALID_INPUT,
+  ERR_last, // Only for indexing, do not use or delete
 };
 
 const char *errorMessages[] = {
-    "",                 // 0: No error
-    "Array is empty.",  // 1: ERR_EMPTY_ARRAY
+    "",                                    // 0: NO_ERROR
+    "Pozitif bir tam sayı değeri giriniz." // 1: ERR_INVALID_INPUT
 };
 
-byte error(byte errorCode) {
-    if (errorCode > 0 && errorCode < ERR_last) {
-        fprintf(stderr, "Error [%d]: %s\n", errorCode, errorMessages[errorCode]);
-        return errorCode;
-    } else {
-        fprintf(stderr, "Unknown error code: %d\n", errorCode);
-        return errorCode;
-    }
+byte printError(int errorCode) {
+  if (errorCode > 0 && errorCode < ERR_last) {
+    fprintf(stderr, "Error [%d]: %s\n", errorCode, errorMessages[errorCode]);
+    return errorCode;
+  } 
+  else {
+    fprintf(stderr, "Unknown error code: %d\n", errorCode);
+    return errorCode;
+  }
+}
+
+/** PROJECT STRUCTS **/
+typedef struct apartment {
+  byte flatNo;
+  byte flatPower;
+} APARTMENT_t;
+
+/** PROJECT CONFIG **/
+#define APT_SIZE 32
+#define DEFAULT_FLAT_POWER 0
+
+/** SYSTEM FUNCTIONS **/
+// Read and dispose of data from stdin until \n or EOF encountered - Return \n or EOF
+int clearBuffer(void) {
+  int ch;
+  while ((ch = getchar()) != '\n' && ch != EOF);
+  return ch;
 }
 
 /** FUNCTION DECLARATIONS **/
-void shift_element(int *pElement);
-void insertion_sort();
-void print_array();
+byte getPowerData(dword*);
+void initApartmentStruct(APARTMENT_t*);
+void setApartmentData(APARTMENT_t*, dword);
+byte showApartments(APARTMENT_t*);
 
 /** MAIN **/
 int main() {
+  dword powerData;
+  APARTMENT_t rovreApt[APT_SIZE];
   
-  // Error Handling
-  if (!arrLength) return error(ERR_EMPTY_ARRAY);
+  initApartmentStruct(rovreApt);
+  while (getPowerData(&powerData) != NO_ERROR);
+  setApartmentData(rovreApt, powerData);
+  showApartments(rovreApt);
 
-  print_array();
-  insertion_sort();
-  print_array();
-
-  return 0;
+  return NO_ERROR;
 }
+
 
 /** PROJECT FUNCTIONS **/
+//Take 32-bit power data value and arrange - Return error code
+byte getPowerData(dword* powerData) {
+  printf("Apartman için 32-bit güç dağıtım bilgisini giriniz: ");
+  if (scanf("%u", powerData) != 1) {
+    clearBuffer();
+    return printError(ERR_INVALID_INPUT);
+  }
+  printf("\nGirilen sayı: %u\n", *powerData);
+  return NO_ERROR;
+}
 
-// Takes one element and shifts until there is no higher value in its left
-void shift_element(int *pElement) {
-  for (int *e = pElement; e > arr && *e < *(e - 1); e--) {
-    // XOR Swap Algorithm
-    *e ^= *(e - 1);
-    *(e - 1) ^= *e;
-    *e ^= *(e - 1);
+//Initialize the apartment structure by assigning each apartment an index No.
+void initApartmentStruct(APARTMENT_t* aprt){
+  for(byte i = 0; i < APT_SIZE; i++){
+      aprt[i].flatNo = i;
+      aprt[i].flatPower = DEFAULT_FLAT_POWER;
   }
 }
 
-// Sorts the array with insertion algorithm
-void insertion_sort() {
-  for (int *e = arr; e < arr + arrLength; e++) {
-    if (*e < *(e - 1))
-      shift_element(e);
-  }
+//Set the struct by given 32-bit power data
+void setApartmentData(APARTMENT_t* aprt, dword data){
+  printf("İşaretsiz, 32-bit genişliğinde ikilik tabandaki karşılığı: \n");
+  for(char i = APT_SIZE-1; i >= 0; i--){
+    byte tempStatus = (data & (1u << i)) >> i;
+    printf("%d", tempStatus );
+    if (i % 8 == 0) printf(" ");
+    aprt[i].flatPower = tempStatus;
+  }    
 }
 
-// Prints the elements of array
-void print_array() {
-  printf("Array: ");
-  for (int *e = arr; e < arr + arrLength; e++) {
-    printf(" %d,", *e);
+//Show all information about apartment - Return totalPowerOn
+byte showApartments(APARTMENT_t* aprt){
+  byte totalPowerOn = 0;
+  printf("\n\n---------------- APARTMAN DURUMLARI ----------------\n\n");
+  for(byte i = 0; i < APT_SIZE; i++){
+      if (aprt[i].flatPower) {
+        printf("Apartman No: %2d Elektrik Durumu: Yanik\n", aprt[i].flatNo);
+        totalPowerOn++;
+      }
+      else {
+        printf("Apartman No: %2d Elektrik Durumu: Sonuk\n", aprt[i].flatNo);
+      }
   }
-  printf("\b \n");
+  printf(" Yanik daire sayisi:%2d   Sonuk daire sayisi:%2d\n", totalPowerOn, APT_SIZE-totalPowerOn);
+  printf("---------------------------------------\n");
+  return totalPowerOn;
 }
